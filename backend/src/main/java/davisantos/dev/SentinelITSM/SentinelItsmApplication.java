66f2f40462d7608fc564dbe4cfd63c2569579d80
Davi Santos <davicoutinho984@gmail.com>
2026-05-18 15:45:50 -0300

    chore: setup initial structure

---
+++ backend/src/main/java/davisantos/dev/SentinelITSM/SentinelItsmApplication.java
@@ -0,0 +1,13 @@
+package davisantos.dev.SentinelITSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelItsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelItsmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/davisantos/dev/SentinelITSM/SentinelItsmApplicationTests.java
@@ -0,0 +1,13 @@
+package davisantos.dev.SentinelITSM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelItsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
